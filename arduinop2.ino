db88adc7a0afaf2d89dabcc3fd4f042934a7fddb
Alex Kajavo <alex.kajavo@ictpro.labra>
2019-12-18 12:58:11 +0200

    case siivous

--- arduinop2.ino
+++ arduinop2.ino
@@ -24,15 +24,17 @@ bool buttonPressed(){
   delay(FILTER);
   return retVal;
 }
-
-void loop(){
-  if(buttonPressed()){
-    switch(valaistusTaso){
+void changeState(){
+      switch(valaistusTaso){
       case LEDOFF:valaistusTaso=LEDON;break;
       case LEDON:valaistusTaso=TASO1;break;
       case TASO1:valaistusTaso=TASO2;break;
       case TASO2:valaistusTaso=LEDOFF;break;
   }
   analogWrite(LED, valaistusTaso);
+}
+void loop(){
+  if(buttonPressed()){
+    changeState();
   }
 }
