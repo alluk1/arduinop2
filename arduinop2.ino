8d19762f9b65d89ffcb9c9b7ab21b39f28a86265
Alex Kajavo <alex.kajavo@ictpro.labra>
2019-12-18 12:49:49 +0200

    Valaistuksentason säätö valmis

--- arduinop2.ino
+++ arduinop2.ino
@@ -1,11 +1,12 @@
 #define LED 6
 #define PAINIKE 5
-#define LEDON HIGH
-#define LEFOFF LOW
-#define TASO1 180
-#define TASO2 50
+#define LEDON 255
+#define LEDOFF 0
+#define TASO1 150
+#define TASO2 60
 #define FILTER 20
 int oldState;//Painikkeen tilojen seurantaa varten
+int valaistusTaso = LEDOFF;
 void setup(){
   pinMode(LED, OUTPUT);
   pinMode(PAINIKE, INPUT_PULLUP);
@@ -26,6 +27,12 @@ bool buttonPressed(){
 
 void loop(){
   if(buttonPressed()){
-    Serial.println("\\o/");
+    switch(valaistusTaso){
+      case LEDOFF:valaistusTaso=LEDON;break;
+      case LEDON:valaistusTaso=TASO1;break;
+      case TASO1:valaistusTaso=TASO2;break;
+      case TASO2:valaistusTaso=LEDOFF;break;
+  }
+  analogWrite(LED, valaistusTaso);
   }
 }
