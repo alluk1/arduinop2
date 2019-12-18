84bb02015ac2d5b863a78f9a6f67548bc272caee
Alex Kajavo <alex.kajavo@ictpro.labra>
2019-12-18 14:56:50 +0200

    Luokka luotu

--- arduinop2.ino
+++ arduinop2.ino
@@ -1,40 +1,12 @@
-#define LED 6
-#define PAINIKE 5
-#define LEDON 255
-#define LEDOFF 0
-#define TASO1 150
-#define TASO2 60
-#define FILTER 20
-int oldState;//Painikkeen tilojen seurantaa varten
-int valaistusTaso = LEDOFF;
-void setup(){
-  pinMode(LED, OUTPUT);
-  pinMode(PAINIKE, INPUT_PULLUP);
-  oldState = digitalRead(PAINIKE);//Alustetaan "muistijälki"
-  Serial.begin(9600);
-}
+#include "fikkari.h"
 
-bool buttonPressed(){
-  bool retVal = false;
-  int x = digitalRead(PAINIKE);
-  if(x == LOW && oldState == HIGH){//Etsitään painikkeen painallus
-    retVal = true;
-  }
-  oldState = x;
-  delay(FILTER);
-  return retVal;
-}
-void changeState(){
-      switch(valaistusTaso){
-      case LEDOFF:valaistusTaso=LEDON;break;
-      case LEDON:valaistusTaso=TASO1;break;
-      case TASO1:valaistusTaso=TASO2;break;
-      case TASO2:valaistusTaso=LEDOFF;break;
-  }
-  analogWrite(LED, valaistusTaso);
+fikkari T1(5, 6);//Luodaan ilmentymä meidän luokasta
+fikkari T2(9, 10);
+fikkari T3(3, 11);
+void setup(){
 }
 void loop(){
-  if(buttonPressed()){
-    changeState();
-  }
+  T1.handle();
+  T2.handle();
+  T3.handle();
 }
