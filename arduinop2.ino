5c35f8f8e74c0faa054d1be46687ccd28040a75e
Alex Kajavo <alex.kajavo@ictpro.labra>
2019-12-18 12:41:43 +0200

    Painike kunnossa

--- arduinop2.ino
+++ arduinop2.ino
@@ -1,16 +1,31 @@
 #define LED 6
 #define PAINIKE 5
-
-void setup(){
-pinMode(LED, OUTPUT);
-pinMode(PAINIKE, INPUT_PULLUP);
-}
+#define LEDON HIGH
+#define LEFOFF LOW
 #define TASO1 180
-#define TASO2 100
+#define TASO2 50
+#define FILTER 20
+int oldState;//Painikkeen tilojen seurantaa varten
+void setup(){
+  pinMode(LED, OUTPUT);
+  pinMode(PAINIKE, INPUT_PULLUP);
+  oldState = digitalRead(PAINIKE);//Alustetaan "muistijälki"
+  Serial.begin(9600);
+}
+
+bool buttonPressed(){
+  bool retVal = false;
+  int x = digitalRead(PAINIKE);
+  if(x == LOW && oldState == HIGH){//Etsitään painikkeen painallus
+    retVal = true;
+  }
+  oldState = x;
+  delay(FILTER);
+  return retVal;
+}
+
 void loop(){
-  if(digitalRead(PAINIKE)==LOW){
-    analogWrite(LED, TASO2);
-  }else{
-    digitalWrite(LED, LOW);
+  if(buttonPressed()){
+    Serial.println("\\o/");
   }
 }
